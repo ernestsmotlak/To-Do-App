774ff8a59236d135af61bb9794a62b45b756596a
ernestsmotlak <ernestsmotlak@gmail.com>
2024-03-18 17:48:44 +0100

    Here. Need to look at a tutorial.
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- to-do-frontend/src/App.js
+++ to-do-frontend/src/App.js
@@ -2,16 +2,13 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './Components/LoginForm';
 import UserSite from './Components/UserSite';
-import Child from './Components/PathTest/Child';
-import Parent from './Components/PathTest/Parent';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-        <Route path='/child' element={<Child />} />
-        <Route path='/parent' element={<Parent />} />
+          <Route path='/' element={<LoginForm />} /> 
           {/* <Route path='/loginform' element={<LoginForm />} />
           <Route path='/user' element={<UserSite />} /> */}
         </Routes>
