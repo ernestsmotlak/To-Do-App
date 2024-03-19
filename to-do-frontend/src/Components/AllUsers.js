619d622a1bb0222a1a80818b25cd214667c2dd75
ernestsmotlak <ernestsmotlak@gmail.com>
2024-03-19 15:02:42 +0100

    Created a component. Deleted an empty folder.
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

---
+++ to-do-frontend/src/Components/AllUsers.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AllUsers = () => {
+  return (
+    <div>AllUsers
+        <br />
+        Fetch all users. <br />
+        Save into a const. <br />
+        Return to App.js (Parent)
+    </div>
+  )
+}
+
+export default AllUsers
