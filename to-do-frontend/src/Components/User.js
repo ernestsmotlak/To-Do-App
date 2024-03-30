68f79b4fe6a2fd5b200753b86c14f4071c24e3fe
ernestsmotlak <ernestsmotlak@gmail.com>
2024-03-30 10:49:41 +0100

    In DeleteTask.js passed a fetchTask function
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- to-do-frontend/src/Components/User.js
+++ to-do-frontend/src/Components/User.js
@@ -91,7 +91,7 @@ const User = () => {
             )}
 
             <AddTask username={username} uuid={fetchedUuid} fetchTasks={fetchTasks} />
-            <DeleteTask username={username} uuid={fetchedUuid}/>
+            <DeleteTask username={username} uuid={fetchedUuid} fetchTasks={fetchTasks} />
         </div>
     );
 };
