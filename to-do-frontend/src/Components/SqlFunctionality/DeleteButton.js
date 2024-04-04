70f70f50dc0f2a96fc3480ca34029e10b59e000d
ernestsmotlak <ernestsmotlak@gmail.com>
2024-04-04 13:51:57 +0200

    Db changes, testing.
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- to-do-frontend/src/Components/SqlFunctionality/DeleteButton.js
+++ to-do-frontend/src/Components/SqlFunctionality/DeleteButton.js
@@ -5,8 +5,37 @@ const DeleteButton = (props) => {
     const passedUsername = props.username;
     const passedTask = props.taskName;
 
+    const submitDeleteTask = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:3000/api/zbrisiTask', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    taskName: passedTask,
+                    uuid: passedUuid,
+                    username: passedUsername,
+                })
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.error);
+            }
+
+            console.log('Succesfully deleted the task: ' + passedTask + ' and uuid: ' + passedUuid);
+            props.fetchTasks();
+
+        } catch (error) {
+            console.error('Erorr deleting task.', error.message);
+        }
+    };
+
     return (
-        <button>
+        <button onClick={submitDeleteTask}>
             {passedTask}
         </button>
     )
