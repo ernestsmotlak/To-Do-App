7b64befd95a138b7e78bea93626613b5c93bd746
ernestsmotlak <ernestsmotlak@gmail.com>
2024-04-15 18:19:08 +0200

    Aligned items in the navbar, the logo image to the left, the random text to the right with d-flex and justify-content-center/start/end
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- to-do-frontend/src/Components/Header.js
+++ to-do-frontend/src/Components/Header.js
@@ -4,10 +4,16 @@ const Header = () => {
     return (
         <nav className="navbar bg-dark-subtle mb-3 border border-3 border-secondary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                    <img className='' src={logoApp} style={{ width: '50px', height: 'auto' }} alt='nevem ki se je zgubla'></img>
-
-                </a>
+                <div class="container">
+                    <div class="row">
+                        <div class="col d-flex justify-content-start">
+                            <img className='' src={logoApp} style={{ width: '50px', height: 'auto' }} alt='nevem ki se je zgubla'></img>
+                        </div>
+                        <div class="col d-flex justify-content-end align-items-center">
+                            <div className=''></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </nav>
     );
