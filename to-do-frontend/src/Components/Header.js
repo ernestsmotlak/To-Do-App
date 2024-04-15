196c6fe82c0ce546ee1db8605157ac556c67f8be
ernestsmotlak <ernestsmotlak@gmail.com>
2024-04-15 18:31:43 +0200

    Changed icon, to a full arrow, added on hover styles.
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- to-do-frontend/src/Components/Header.js
+++ to-do-frontend/src/Components/Header.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import logoApp from './LogoToDoApp.svg';
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { useNavigate } from 'react-router-dom';
+import './Header.css';
 const Header = () => {
+    const navigate = useNavigate();
+
+    const goToLoginForm = () => {
+        navigate('/');
+    }
+
     return (
         <nav className="navbar bg-dark-subtle mb-3 border border-3 border-secondary">
             <div className="container-fluid">
                 <div class="container">
                     <div class="row">
                         <div class="col d-flex justify-content-start">
-                            <img className='' src={logoApp} style={{ width: '50px', height: 'auto' }} alt='nevem ki se je zgubla'></img>
+                            <img src={logoApp} style={{ width: '50px', height: 'auto' }} alt='nevem ki se je zgubla'></img>
                         </div>
                         <div class="col d-flex justify-content-end align-items-center">
-                            <div className=''></div>
+                            <div>
+                                <i class="bi bi-arrow-left-square-fill custom-right-arrow" style={{ fontSize: 'xx-large', cursor: 'pointer' }} onClick={goToLoginForm}></i>
+                            </div>
                         </div>
                     </div>
                 </div>
