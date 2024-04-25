a376e711568f2e785c9614d58406aa8c8f4c80cd
ernestsmotlak <ernestsmotlak@gmail.com>
2024-04-25 13:05:00 +0200

    Deleted the DesktopUser component, as there is no need for it.
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- to-do-frontend/src/App.js
+++ to-do-frontend/src/App.js
@@ -4,7 +4,6 @@ import LoginForm from './Components/LoginForm';
 import { useEffect, useState } from 'react';
 import NotFound from './Components/NotFound';
 import User from './Components/User';
-import DesktopUser from './Components/DesktopUser';
 
 function App() {
   const [uuidArray, setUuidArray] = useState([]);
@@ -42,7 +41,6 @@ function App() {
             <Route key={index} path={`username/${uniqueUserID}`} element={<User />} />
           ))}
 
-          <Route path='/test' element={<DesktopUser />} />
         </Routes>
       </div>
     </Router>
